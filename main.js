967599706548d291509a7313cfc714050306bc38
suzu-novel <suzu0427su@gmail.com>
2026-02-17 19:50:46 +0900

    main.js の更新

--- main.js
+++ main.js
@@ -1,37 +1,20 @@
+// 鈴の音
 function playSound() {
     const bell = document.getElementById("bell");
     bell.play();
 }
 
-const title = document.getElementById("title");
-
-title.style.opacity = 0;
-
-setTimeout(() => {
-    title.style.transition = "opacity 3s";
-    title.style.opacity = 1;
-}, 500);
-
+// タイトル画面クリックでメニュー表示
 window.addEventListener("load", () => {
 
     const logoArea = document.getElementById("logo-area");
     const menuArea = document.getElementById("menu-area");
     const titleScreen = document.getElementById("title-screen");
 
-    // 最初にスクロール禁止
-    document.body.classList.add("lock");
-
     logoArea.addEventListener("click", () => {
 
-        // メニュー表示
         menuArea.classList.add("show");
-
-        // スクロール解放
-        document.body.classList.remove("lock");
-
-        // タイトル画面を通常レイアウトに戻す
-        titleScreen.style.position = "relative";
-        titleScreen.style.height = "auto";
+        titleScreen.classList.add("hide");
 
     });
 
