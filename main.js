38866a96f8116278aef906dfc93c25b2ba23c76f
suzu-novel <suzu0427su@gmail.com>
2026-02-17 19:48:46 +0900

    main.js の更新

--- main.js
+++ main.js
@@ -18,13 +18,20 @@ window.addEventListener("load", () => {
     const menuArea = document.getElementById("menu-area");
     const titleScreen = document.getElementById("title-screen");
 
+    // 最初にスクロール禁止
+    document.body.classList.add("lock");
+
     logoArea.addEventListener("click", () => {
 
         // メニュー表示
         menuArea.classList.add("show");
 
-        // タイトル画面を通常ページに戻す
-        titleScreen.classList.add("hide");
+        // スクロール解放
+        document.body.classList.remove("lock");
+
+        // タイトル画面を通常レイアウトに戻す
+        titleScreen.style.position = "relative";
+        titleScreen.style.height = "auto";
 
     });
 
