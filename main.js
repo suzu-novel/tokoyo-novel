7327a084f2554fe26e9e054d036bc7dc9dec46a2
suzu-novel <suzu0427su@gmail.com>
2026-02-18 19:15:51 +0900

    main.js の更新

--- main.js
+++ main.js
@@ -9,16 +9,11 @@ window.addEventListener("DOMContentLoaded", () => {
         // メニュー表示
         menuArea.classList.add("show");
 
-        // フェードアウト
-        titleScreen.classList.add("hide");
-
-        // フェード後に完全削除
-        setTimeout(() => {
-            titleScreen.style.display = "none";
-        }, 1000);
-
         // スクロール解除
         document.body.style.overflow = "auto";
+
+        // タイトル画面を完全削除（←これが重要）
+        titleScreen.remove();
 
     });
 
