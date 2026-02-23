3754ec49668780776be15f456a4599dfa3d63245
suzu-novel <suzu0427su@gmail.com>
2026-02-23 14:25:26 +0900

    main.js の更新

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ window.addEventListener("DOMContentLoaded", function () {
 
     const logoArea = document.getElementById("logo-area");
     const titleScreen = document.getElementById("title-screen");
+    const menuArea = document.getElementById("menu-area");
 
     logoArea.addEventListener("click", function () {
 
-        // 完全に消す
+        // タイトル消す
         titleScreen.style.display = "none";
+
+        // メニュー表示
+        menuArea.classList.add("show");
 
         // スクロール解除
         document.body.style.overflow = "auto";
